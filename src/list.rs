14c609b2abebd52243950647f1a090394d672615
Beefy1337 <45110365+Beefy1337@users.noreply.github.com>
2023-03-13 23:31:03 +0100

    make list(..) not expect 'info' subdir to exist

--- src/list.rs
+++ src/list.rs
@@ -2,7 +2,13 @@ use std::path::Path;
 use crate::trash_info::Entry;
 
 pub fn list(trash_dir: &Path) -> anyhow::Result<Vec<anyhow::Result<Entry>>> {
-    let entries = std::fs::read_dir(trash_dir.join("info"))?
+    let info = trash_dir.join("info");
+
+    if !info.exists() {
+        return Ok(vec![]);
+    }
+
+    let entries = std::fs::read_dir(info)?
         .map(|file| -> anyhow::Result<Entry> {
             let file = file?;
             Entry::from_file(&file.path())
