82ff3f9d1ad73e082d97e93faf360c926e116c1c
Beefy1337 <45110365+Beefy1337@users.noreply.github.com>
2023-03-14 01:56:53 +0100

    remove unused CustomError variant

--- src/errors.rs
+++ src/errors.rs
@@ -5,9 +5,6 @@ pub enum CustomError {
     #[error("{0} could not be parsed")]
     FileParseFailed(String),
 
-    #[error(".trashinfo file at {0} is missing its corresponding trashed file")]
-    TrashInfoMissing(String),
-
     #[error(".trashinfo at {0} is invalid, could not parse")]
     TrashInfoParseFailure(String),
 
